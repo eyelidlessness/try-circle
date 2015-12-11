05c71997aee0ba197b8a69eabed5ad16c0edb138
eyelidlessness <gnosis@gmail.com>
2015-12-10 22:30:58 -0500

    Try again, with region
    Also rearranged and created some vars to make editing this thing easier

--- elasticbeanstalk.sh
+++ elasticbeanstalk.sh
@@ -2,16 +2,22 @@
 
 SHA1=$1
 
+IMAGE=eyelidlessness/try-circle
+
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+EB_BUCKET=elasticbeanstalk-us-west-2-350775868452
+EB_ENVIRONMENT=Default-Environment
+
+echo $'[default]\nregion=us-west-2' > ~/.aws/config
+
 # Deploy image to Docker Hub
-docker push eyelidlessness/try-circle:$SHA1
+docker push $IMAGE:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=elasticbeanstalk-us-west-2-350775868452
-DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name try-circle \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name Default-Environment --version-label $SHA1
+aws elasticbeanstalk update-environment --environment-name $EB_ENVIRONMENT --version-label $SHA1
