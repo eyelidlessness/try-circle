271c2d1665e11f61c4c614e3cca890ce8a9fea50
eyelidlessness <gnosis@gmail.com>
2015-12-10 22:55:19 -0500

    Banana gif

--- src/try_circle/wat.cljs
+++ src/try_circle/wat.cljs
@@ -4,8 +4,10 @@
   (js/console.log "Wat?" wat))
 
 (defn a-change! []
-  (let [p (js/document.createElement "p")]
-    (set! (.-innerHTML p) "UPDATED!")
+  (let [p (js/document.createElement "p")
+        img (js/document.createElement "img")]
+    (set! (.-src img) "/img/banana.gif")
+    (.appendChild p img)
     (js/document.body.appendChild p)))
 
 (a-change!)
