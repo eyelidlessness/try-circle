f9b4b6834b7f98f2700ee89713a27aa2f9aa4296
eyelidlessness <gnosis@gmail.com>
2015-12-10 22:37:02 -0500

    Yeah I keep missing stuff after I rolled a new AWS acct

--- elasticbeanstalk.sh
+++ elasticbeanstalk.sh
@@ -6,6 +6,7 @@ IMAGE=eyelidlessness/try-circle
 
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 EB_BUCKET=elasticbeanstalk-us-west-2-350775868452
+EB_APPLICATION="My First Elastic Beanstalk Application"
 EB_ENVIRONMENT=Default-Environment
 
 echo $'[default]\nregion=us-west-2' > ~/.aws/config
@@ -16,7 +17,7 @@ docker push $IMAGE:$SHA1
 # Create new Elastic Beanstalk version
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name try-circle \
+aws elasticbeanstalk create-application-version --application-name $EB_APPLICATION \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
